9b838d6d515ec1685ddca794fbeb56950e1d0447
ryanluoxu <luoxu2011@gmail.com>
2018-02-10 00:35:18 +0800

    add PathChecker

---
+++ RyanDriverPool/src/main/PathChecker.java
@@ -0,0 +1,51 @@
+package main;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathChecker {
+
+	public static void main(String[] args) throws IOException {
+		if ((args != null) && (args.length == 1)) {
+			Path path = Paths.get(args[0]);
+			File file = new File(args[0]);
+			if (Files.exists(path) && !Files.notExists(path)) {
+				System.out.println(path + " exists..");
+			} else if(!Files.exists(path) && !Files.notExists(path)) {
+				System.err.println(">>> access denied..");
+			} else {	
+				System.out.println("path not exist..");
+				String fileName = String.valueOf(path.getFileName());
+				if (fileName.contains(".")) {
+					// create file
+					Path dir = path.getParent();
+					if (Files.exists(dir)) {
+						System.out.println("dir exists. creating " + path.getFileName());
+						file.createNewFile();
+					} else {
+						System.out.println("dir not exist. creating " + path);
+						file.getParentFile().mkdirs();
+						file.createNewFile();
+					}
+				} else {
+					System.out.println("Creating " + file);
+					file.mkdir();
+				}
+			}
+		} else {
+			System.err.println(">>> Usage: <program> <path>..");
+		}
+	}
+	
+	/**
+	 * Test 1 - existed file - D:\Dropbox\programmer\blog\debug.log - pass
+	 * Test 2 - not existed file - D:\Dropbox\programmer\blog\test.txt - pass
+	 * Test 3 - existed folder - D:\Dropbox\programmer\blog - pass
+	 * Test 4 - existed file - D:\Dropbox\programmer\blog\testFolder - pass
+	 * Test 5 - not existed dir and file - D:\Dropbox\programmer\blog\testFolder\test.txt - pass
+	 */
+
+}
---
+++ RyanDriverPool/src/test/Test.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		String pathString = "D:\\home\\user\\test";
+		
+		Path path = Paths.get(pathString);
+		
+		System.out.println(path.getNameCount());
+		System.out.println(path.getFileName());
+		System.out.println(path.getParent());
+		System.out.println(path.getRoot());
+		
+
+	}
+}
