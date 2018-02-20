cc4d6d8dd0a65319bdca3c976abf5b9f95d3aaff
melvin <melvintan1985.mm@gmail.com>
2018-02-20 22:58:45 +0800

    update bat to give nicer log

--- MelvinDriverPool/src/tmp/runJAR.bat
+++ MelvinDriverPool/src/tmp/runJAR.bat
@@ -1,10 +1,15 @@
+@echo off
 SET TARGET_APPLICATION_DIRECTORY=%1
 SET FILENAME=%2
 SET LOG_DIRECTORY=%TARGET_APPLICATION_DIRECTORY%
 CD %TARGET_APPLICATION_DIRECTORY%
 
 SET LOGFILE=%LOG_DIRECTORY%\runJAR_LogFile.log
-
+@echo on
 @echo start execution of runJAR bat file > %LOGFILE%
+@echo ----------------------------------- >> %LOGFILE%
+@echo TARGET_APPLICATION_DIRECTORY=%TARGET_APPLICATION_DIRECTORY% >> %LOGFILE%
+@echo FILENAME=%FILENAME% >> %LOGFILE%
+@echo ----------------------------------- >> %LOGFILE%
 java -jar %FILENAME% >> %LOGFILE%
 @echo end >> %LOGFILE%
